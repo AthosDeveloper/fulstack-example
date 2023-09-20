69b235c03f097059aa701a627dea28be4bf2687c
AthosDeveloper <athos.silva@zup.com.br>
2023-09-20 17:36:03 -0300

    starting project

---
+++ src/main/java/com/frontEndExample/fulstackProject/FulstackProjectApplication.java
@@ -0,0 +1,13 @@
+package com.frontEndExample.fulstackProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulstackProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulstackProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frontEndExample/fulstackProject/FulstackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frontEndExample.fulstackProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulstackProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
